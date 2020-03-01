f6e0cefce5840b6ba312445069204febefda2c08
Matěj Ošanec <matej.osanec@geewa.com>
2020-03-01 21:38:04 +0100

    Updated Unity Editor version to 2019.1.6f1

--- Assets/Scripts/CKIEditor/Common/CkiConsts.cs
+++ Assets/Scripts/CKIEditor/Common/CkiConsts.cs
@@ -4,5 +4,7 @@ namespace CKIEditor
     {
         public static int INSTRUMENT_NAME_CHARACTER_LIMIT = 9;
         public static int CC_NAME_CHARACTER_LIMIT = 6;
+        
+        public static int TRACK_VALUES_PER_SCREEN = 6;
     }
 }
